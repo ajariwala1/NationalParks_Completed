a4f959fa4a026a1683c9c95af695a68951492781
Abhishek Jariwala <119805313+ajariwala1@users.noreply.github.com>
2025-07-15 19:15:16 -0700

    Initial Commit

---
+++ NationalParks_Completed/NationalParks_CompletedApp.swift
@@ -0,0 +1,17 @@
+//
+// NationalParks_CompletedApp.swift : NationalParks_Completed
+//
+// Copyright © 2025 Auburn University.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct NationalParks_CompletedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
